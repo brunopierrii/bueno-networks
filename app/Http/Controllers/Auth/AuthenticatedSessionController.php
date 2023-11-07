9b5de7cc95f44a5e476148e74ce101b99f15a544
Bruno Pierri <pierri95@gmail.com>
2023-11-07 17:39:13 -0300

    modificando action que chama a view de login, para criar usuário admin(com role admin) para acesso ao sistema - modificação usada para teste

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 use Illuminate\View\View;
 
 class AuthenticatedSessionController extends Controller
@@ -17,6 +19,18 @@ class AuthenticatedSessionController extends Controller
      */
     public function create(): View
     {
+        $user = User::where('email', 'admin@admin.com')->first();
+
+        if(is_null($user)){
+            $user = User::factory()->create([
+                'name' => 'Admin',
+                'email' => 'admin@admin.com',
+                'password' => 'admin123'
+            ]);
+
+            $user->assignRoles('admin');
+        }
+
         return view('auth.login');
     }
 
@@ -25,11 +39,16 @@ class AuthenticatedSessionController extends Controller
      */
     public function store(LoginRequest $request): RedirectResponse
     {
-        $request->authenticate();
+        try{
+            $request->authenticate();
 
-        $request->session()->regenerate();
+            $request->session()->regenerate();
 
-        return redirect()->intended(RouteServiceProvider::HOME);
+            return redirect()->intended(RouteServiceProvider::HOME);
+
+        } catch(ValidationException $validator){
+            return redirect('/login')->with('msg', $validator->getMessage());
+        }       
     }
 
     /**
