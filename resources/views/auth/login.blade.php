be273e49bebc5ef5d7aeb6ac2e3d2ccf6a363d5e
Bruno Pierri <pierri95@gmail.com>
2023-11-07 17:42:26 -0300

    alteração na página de login, inserindo mensagem feedback de error, e inserindo evento onclick para submeter o formulário

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,30 +2,37 @@
 
 
 @section('content')
+    @dump()
 
-<div class="col-md-12 login-container">
-    <img id="img-logo" class="mb-4" style="margin: 0 auto;" src="/img/logo-taskier-light.png" alt="logo-taskier">
+    <div class="col-md-12 login-container">
+        <img id="img-logo" class="mb-4" style="margin: 0 auto;" src="/img/logo-taskier-light.png" alt="logo-taskier">
 
-    <form action="/login" method="POST" class="align-self-center">
-        @csrf
-        <div class="form-floating mb-3">
-            <input id="email" class="form-control" type="email" name="email" required autofocus />
-            <label for="email">Email</label>
-        </div>
-        <div class="form-floating mb-3">
-            <input id="password" class="form-control" type="password" name="password" required />
-            <label for="password">Password</label>
-        </div>
-        <div class="form-check mb-3">
-            <label class="form-check-label" for="remember_me">Remember-me</label>
-            <input id="remember_me" class="form-check-input" type="checkbox" name="remember_me"  />
-        </div>
+        <form action="/login" method="POST" class="align-self-center">
 
-        <div class="d-grid gap-2 col-6 mx-auto">
-            <button type="submit" id="btn-login" class="btn">Entrar</button>
-        </div>
-    </form>
-</div>
+            @if (session('msg'))
+                <div class="alert alert-danger" role="alert">
+                    {{ session('msg') }}
+                </div>
+            @endif
 
+            @csrf
+            <div class="form-floating mb-3">
+                <input id="email" class="form-control" type="email" name="email" required autofocus />
+                <label for="email">Email</label>
+            </div>
+            <div class="form-floating mb-3">
+                <input id="password" class="form-control" type="password" name="password" required />
+                <label for="password">Password</label>
+            </div>
+            <div class="form-check mb-3">
+                <label class="form-check-label" for="remember_me">Remember-me</label>
+                <input id="remember_me" class="form-check-input" type="checkbox" name="remember_me" />
+            </div>
+
+            <div class="d-grid gap-2 col-6 mx-auto">
+                <button onclick="event.preventDefault();this.closest('form').submit();" id="btn-login"
+                    class="btn btns-submit">Entrar</button>
+            </div>
+        </form>
+    </div>
 @endsection
-
