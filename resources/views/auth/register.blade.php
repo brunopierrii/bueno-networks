f9319a7d3c0be1664bacce05e73ced38a4e7fbb2
Bruno Pierri <pierri95@gmail.com>
2023-11-11 05:41:05 -0300

    alteracoes de config

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,59 +1,66 @@
 @extends('layouts.guest')
-@include('layouts.navigation-auth')
 
+<div class="col-md-12 login-container">
+    <img id="img-logo" class="mb-4" style="margin: 0 auto;" src="/img/logo-taskier-light.png" alt="logo-taskier">
 
-
-
-{{-- <x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
+    <form action="/register" method="POST" class="align-self-center">
         @csrf
 
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+        <div class="form-floating mb-3">
+            <input id="name" class="form-control" type="text" name="name" value="{{ old('name') }}"
+                required autofocus />
+            <label for="name">Name</label>
+        </div>
+        <div class="form-floating mb-3">
+            <input id="email" class="form-control" type="email" value="{{ old('email') }}" name="email"
+                required autofocus />
+            <label for="email">Email</label>
+        </div>
+        <div class="form-floating mb-3">
+            <input id="password" class="form-control" type="password" name="password" required />
+            <label for="password">Password</label>
+        </div>
+        <div class="form-floating mb-3">
+            <input id="password_confirmation" class="form-control" type="password" name="password_confirmation"
+                required />
+            <label for="password_confirmation">Confirm Password</label>
         </div>
 
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ml-4">
-                {{ __('Register') }}
-            </x-primary-button>
+        <div class="d-flex flex-row-reverse align-items-center">
+            <button type="submit" class="btn">Cadastrar</button>
+            <a class="nav-link btns-nav mx-2" href="/login">Já é cadastrado?</a>
         </div>
     </form>
-</x-guest-layout> --}}
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
