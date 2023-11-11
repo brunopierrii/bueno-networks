6ef4a06bc11bba8d572d3d3b043ff3e645391b87
Bruno Pierri <pierri95@gmail.com>
2023-11-11 05:19:34 -0300

    inserindo caminho do service-worker.js no webpack.mix.js para ser transpilado

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,5 +6,6 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
    .js('resources/js/bootstrap.js', 'public/js')
+   .js('resources/js/service-worker.js', 'public/js')
    .postCss('resources/css/app.css', 'public/css');
 
